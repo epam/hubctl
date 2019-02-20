f4740d7ab0be606edce263aa2a1d6478715e191c
Arkadi Shishlov <arkadi@agilestacks.com>
2019-02-20 13:18:16 -0500

    More EKS regions

--- src/hub/aws/eks.go
+++ src/hub/aws/eks.go
@@ -12,7 +12,8 @@ import (
 )
 
 var (
-	eksSupportedRegions = []string{"us-east-1", "us-west-2"}
+	eksSupportedRegions = []string{"us-east-1", "us-east-2", "us-west-2", "eu-west-1", "eu-central-1", "eu-north-1",
+		"ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2"}
 )
 
 func DescribeEKSCluster(region, name string) (string, []byte, error) {
