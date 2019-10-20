3c62bb74b4f9f9115b95b7fcc8c3bc7916f5a4be
Arkadi Shishlov <arkadi@agilestacks.com>
2019-10-20 23:53:44 +0300

    Bump helm binary version requirement

--- src/hub/lifecycle/requirement.go
+++ src/hub/lifecycle/requirement.go
@@ -169,7 +169,7 @@ var binVersion = map[string]BinVersion{
 	"gsutil":  {"4.38", regexp.MustCompile("version: ([\\d.]+)")},
 	"vault":   {"1.1.2", regexp.MustCompile("Vault v([\\d.]+)")},
 	"kubectl": {"1.14.7", regexp.MustCompile("GitVersion:\"v([\\d.]+)")},
-	"helm":    {"2.14.3", regexp.MustCompile("SemVer:\"v([\\d.]+)")},
+	"helm":    {"2.15.0", regexp.MustCompile("SemVer:\"v([\\d.]+)")},
 }
 
 func checkStackRequires(requires []string, optional, requiresOfOptionalComponents map[string][]string) map[string][]string {
