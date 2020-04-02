7b464baefb4f1f5711e6287060d061852e0961db
Arkadi Shishlov <arkadi@agilestacks.com>
2020-04-02 21:16:50 +0300

    Instance search example template name

--- examples/instance-search/create.sh
+++ examples/instance-search/create.sh
@@ -8,7 +8,7 @@ kube_dashboard_versions=(2.0.0-rc3 2.0.0-rc6)
 
 for i in $(seq 1 50); do
     name=eks-$i
-    hub api cluster create eks $name t3.medium 1 3 -e Demo01-smoke-arkadi -m K8s --kube-dashboard --eks-admin arkadi -s 0.05 -y
+    hub api cluster create eks $name t3.medium 1 3 -e Demo01-smoke-arkadi -m EKS --kube-dashboard --eks-admin arkadi -s 0.05 -y
     t=${traefik_versions[$(($RANDOM % 2))]}
     d=${kube_dashboard_versions[$(($RANDOM % 2))]}
     cat $(dirname $0)/patch.json |
