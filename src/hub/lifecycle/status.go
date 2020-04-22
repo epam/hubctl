d5768c36ed3a409d65f26af8ef8afd52589c4760
Arkadi Shishlov <arkadi@agilestacks.com>
2020-04-22 14:36:24 +0300

    State stack status in case there are no (deployed) mandatory components

--- src/hub/lifecycle/status.go
+++ src/hub/lifecycle/status.go
@@ -9,33 +9,53 @@ import (
 )
 
 func calculateStackStatus(stackManifest *manifest.Manifest, stateManifest *state.StateManifest, verb string) (string, string) {
-	statuses := make(map[string][]string)
-	mandatoryComponents := 0
+	var statuses map[string][]string
+	mandatory := make(map[string][]string)
+	optional := make(map[string][]string)
+	mandatoryCount := 0
+	optionalCount := 0
+
 	for _, componentName := range stackManifest.Lifecycle.Order {
-		if !optionalComponent(&stackManifest.Lifecycle, componentName) {
-			mandatoryComponents++
-			componentState, exist := stateManifest.Components[componentName]
-			if exist {
-				componentStatus := componentState.Status
-				if componentStatus == "" { // compat
-					componentStatus = "deployed"
-				}
-				util.AppendMapList(statuses, componentStatus, componentName)
-			}
+		if optionalComponent(&stackManifest.Lifecycle, componentName) {
+			optionalCount++
+			statuses = optional
+		} else {
+			mandatoryCount++
+			statuses = mandatory
 		}
-	}
-	if mandatoryComponents == 0 {
-		return verb + "ed", ""
-	}
-	for _, candidate := range []string{"deployed", "undeployed"} {
-		components, exist := statuses[candidate]
-		if exist && len(components) == mandatoryComponents {
-			return candidate, ""
+		componentState, exist := stateManifest.Components[componentName]
+		if exist {
+			componentStatus := componentState.Status
+			if componentStatus == "" { // compat
+				componentStatus = "deployed"
+			}
+			util.AppendMapList(statuses, componentStatus, componentName)
 		}
 	}
 
-	// should force-undeployed stack become undeployed?
-	return "incomplete",
-		fmt.Sprintf("Mandatory components state:\n%s",
-			util.SprintDeps(statuses))
+	optionalStatus := "undeployed"
+	if optionalCount >= 0 {
+		if _, exist := optional["deployed"]; exist {
+			optionalStatus = "deployed"
+		}
+	}
+
+	if mandatoryCount >= 0 {
+		for _, candidate := range []string{"deployed", "undeployed"} {
+			if components, exist := mandatory[candidate]; exist && len(components) == mandatoryCount {
+				// if all mandatory components are deployed then the stack status is deployed
+				// if all mandatory components are undeployed then the stack status is undeployed only if
+				// there are no deployed optional components
+				if candidate == "deployed" || optionalStatus == "undeployed" {
+					return candidate, ""
+				}
+			}
+		}
+		// should force-undeployed stack become undeployed?
+		return "incomplete",
+			fmt.Sprintf("Mandatory components state:\n%s",
+				util.SprintDeps(statuses))
+	}
+
+	return optionalStatus, ""
 }
