430a4352d290ef73b02e421f8facada8a902ec1d
Viktors Oginskis <viktors_oginskis@epam.com>
2022-06-29 16:52:01 +0300

    License changed to Mozilla PL V2

--- examples/eks/import.sh
+++ examples/eks/import.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Copyright (c) 2022 EPAM Systems, Inc.
+# 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 hub api import eks arkadi352 -e Dev -w \
     --eks-endpoint 80A1E4C2EA4367436CA445DAA313204B.yl4.us-east-1.eks.amazonaws.com < examples/eks/arkadi352-ca-cert.pem
--- examples/platformless-scratch/stack.sh
+++ examples/platformless-scratch/stack.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Copyright (c) 2022 EPAM Systems, Inc.
+# 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 hub api template create < examples/platformless-scratch/template.json
 hub api template init S3
