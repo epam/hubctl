be5a405a5114599939396e2b0f11e750a721fae5
Antons Kranga <akranga@gmail.com>
2023-01-16 14:05:44 +0200

    ARM probe unit test epam/hub-extensions#36

---
+++ cmd/hub/lifecycle/probe_test.go
@@ -0,0 +1,44 @@
+package lifecycle
+
+import (
+	"testing"
+
+	"github.com/epam/hubctl/cmd/hub/manifest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProbeARM(t *testing.T) {
+	requiresFoo := manifest.Manifest{
+		Requires: []string{"foo"},
+	}
+
+	found, err := probeImplementation("./foo", "deploy", &requiresFoo)
+	if err != nil {
+		assert.Errorf(t, err, "When 'foo' in requires is not a well known, should not find implementation and fail with error", err)
+	}
+	assert.Falsef(t, found, "When 'foo' in requires is not a well known, should not find implementation and fail with error")
+
+	requiresARM := manifest.Manifest{
+		Requires: []string{"arm"},
+	}
+	found, err = probeImplementation("./foo", "deploy", &requiresARM)
+	if err != nil {
+		assert.NoErrorf(t, err, "When 'arm' in component requires, it should resolve to implementation %v", err)
+	}
+	assert.Truef(t, found, "When 'arm' in component requires, it should resolve to implementation")
+}
+
+// ARM extension should be called when component requires 'arm' in the manifest
+func TestFindARMImplementation(t *testing.T) {
+	requiresARM := manifest.Manifest{
+		Requires: []string{"arm"},
+	}
+	extension, err := findImplementation("./foo", "deploy", &requiresARM)
+	if err != nil {
+		assert.Fail(t, "When 'arm' in component requires, it should resolve to extension. Error is not expected here")
+	}
+	assert.NotNil(t, extension, "When 'arm' in component requires, it should resolve to the extension")
+
+	assert.Contains(t, extension.Path, "/hub-component-arm", "ARM extension should be returned")
+	assert.FileExists(t, extension.Path, "ARM should extension should be found in the file system")
+}
